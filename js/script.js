c13a819de8433deae50047894a3103f13fc49615
vadim <vadim.nagirniak@aiesec.net>
2015-08-15 12:33:13 +0300

    first commit

--- js/script.js
+++ js/script.js
@@ -1,2 +1,18 @@
 $(document).ready(function(){
+	// Script fot header 
+    $('.link-menu').click(function(e){
+    	e.preventDefault(); // If this method is called, the default action of the event will not be triggered.
+    
+    	var popup = $(this).attr('href');
+
+        $(this).toggleClass('active').siblings('.link-menu').removeClass('active');
+    	$(popup).fadeToggle().siblings('.popup').fadeOut();
+	});
+
+	$(document).on('click', function(e) {
+    	if (!$(e.target).closest('.popup,.link-menu').length) {
+        	$('.popup').fadeOut();
+            $('.link-menu').removeClass('active');
+    	}
+	});
 });
